be0b0ba1cd6abc20305fd6d07a711ec9bd591bb6
Raphael Zimeray <raphaelzimeray@me.com>
2022-11-10 14:22:38 +0100

    new is done

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,12 +2,15 @@ class BookmarksController < ApplicationController
 
   def new
     @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = List.find(params[:list_id])
     @list = List.find(params[:list_id])
+    @movie = Movie.find(params[:bookmark][:movie])
+    @bookmark.movie = @movie
     if @bookmark.save
       redirect_to list_path(@list)
     else
