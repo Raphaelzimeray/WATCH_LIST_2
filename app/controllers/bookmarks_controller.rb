67ba80678f16230dadf8092f43064415ae85e6db
Raphael Zimeray <raphaelzimeray@me.com>
2022-11-10 11:20:33 +0100

    new

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,23 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = List.find(params[:list_id])
+    @list = List.find(params[:list_id])
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+end
