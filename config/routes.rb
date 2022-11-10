307c044bc3c8a1a8e50aa805246de6dd0a62fbe2
Raphael Zimeray <raphaelzimeray@me.com>
2022-11-10 18:00:38 +0100

    cest fait

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :lists, only: [:index, :show, :new, :create] do
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
     resources :bookmarks, only: [:new, :create ]
   end
   resources :bookmarks, only: [ :destroy ]
